2d81972e456f6147c85f8acc4ffcb63f4765d53c
humbertogfs <humbertogfs55@proton.me>
2025-05-13 20:20:31 -0300

    feito esse

--- Trabalho-2/main.py
+++ Trabalho-2/main.py
@@ -1,13 +1,15 @@
 from mm1_queue import mm1_queue_metrics
 from mmc_queue import mmc_queue_metrics
 from mm1k_queue import mm1k_queue_metrics
+from mm1_finitePopulation import mm1_finite_population_metrics
 
 def display_menu():
     print("\n--- Teoria das Filas ---")
     print("1. Modelo M/M/1")
-    print("2. Modelo M/M/c")
-    print("3. Modelo M/M/1/K")
-    print("4. Sair")
+    print("2. Modelo M/M/1 População finita")
+    print("3. Modelo M/M/c")
+    print("4. Modelo M/M/1/K")
+    print("5. Sair")
     print("------------------------")
 
 def handle_mm1():
@@ -21,6 +23,21 @@ def handle_mm1():
     else:
         for metric, value in metrics.items():
             print(f"{metric}: {value:.4f}")
+
+def handle_mm1_finite():
+    print("\n--- Modelo M/M/1 População finita ---")
+    arrival_rate = float(input("Digite a taxa de chegada (λ): "))
+    service_rate = float(input("Digite a taxa de serviço (μ): "))
+    population_size = int(input("Digite o tamanho da população (N): "))
+    metrics = mm1_finite_population_metrics(arrival_rate, service_rate, population_size)
+    if "Erro" in metrics:
+        print(metrics["Erro"])
+    else:
+        for metric, value in metrics.items():
+            if isinstance(value, list):
+                print(f"{metric}: {', '.join(f'{v:.4f}' for v in value)}")
+            else:
+                print(f"{metric}: {value:.4f}")
 
 def handle_mmc():
     print("\n--- Modelo M/M/c ---")
@@ -49,14 +66,16 @@ def handle_mm1k():
 def main():
     while True:
         display_menu()
-        choice = input("Escolha uma opção (1-4): ").strip()
+        choice = input("Escolha uma opção (1-5): ").strip()
         if choice == "1":
             handle_mm1()
         elif choice == "2":
-            handle_mmc()
+            handle_mm1_finite()
         elif choice == "3":
-            handle_mm1k()
+            handle_mmc()
         elif choice == "4":
+            handle_mm1k()
+        elif choice == "5":
             print("Saindo do programa. Até logo!")
             break
         else:
